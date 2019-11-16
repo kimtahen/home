69339303a97af07064e0b16e9192af746eedd1d4
kimtahen <kimtahen@naver.com>
2019-11-16 22:41:04 +0900

    Updates

---
+++ precache-manifest.f9cdda30f04c1f5c3a0489ed219daa8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f383de551305c76132632c95aa24e8",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "caea466b94decc856854",
+    "url": "/home/static/css/main.51f09f78.chunk.css"
+  },
+  {
+    "revision": "ef528f3072e8f7245865",
+    "url": "/home/static/js/2.dd749b1f.chunk.js"
+  },
+  {
+    "revision": "caea466b94decc856854",
+    "url": "/home/static/js/main.055ebeda.chunk.js"
+  },
+  {
+    "revision": "c72b80550a507689451b",
+    "url": "/home/static/js/runtime-main.81dffebc.js"
+  },
+  {
+    "revision": "d5ac954ef926f8b7106d2d85fa670354",
+    "url": "/home/static/media/logo_big.d5ac954e.svg"
+  }
+]);
